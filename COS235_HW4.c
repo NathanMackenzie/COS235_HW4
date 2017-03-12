219e912727a848a0b24f4cc2cee037056daff579
weemackee <weemackee@gmail.com>
2017-03-12 12:26:28 -0400

    Initial commit

---
+++ COS235_HW4.c
@@ -0,0 +1,11 @@
+#include <stdio>
+
+int ISSET(unsigned char BitSet, int BitPos);
+void ADDBIT(unsigned char *BitSet, int BitPos);
+void DELBIT(unsigned char *BitSet, int BitPos);
+void PrintBits(unsigned char BitSet);
+
+int main(){
+	
+	return 0;
+}
